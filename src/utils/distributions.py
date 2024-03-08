225e5d7a78d1afd0faf57ccc1d6f96c8f3e36b95
Joshua Stiller <joshua.stiller@tum.de>
2024-03-08 19:47:43 +0100

    DOC: Added comments and Docstrings to the distributions file

--- src/utils/distributions.py
+++ src/utils/distributions.py
@@ -10,14 +10,53 @@ from normflows.distributions import BaseDistribution
 
 
 class JointDistribution(BaseDistribution):
-    def __init__(self, distributions, dimensions):
+    """
+    Defines a Normalizing Flow distribution that is a joint distribution of multiple independent distributions.
+
+    Parameters
+    ----------
+    distributions: list,
+        A list of distributions.
+    dimensions: list,
+        A list of dimensions of the distributions.
+
+    """
+    def __init__(self, distributions: list[BaseDistribution], dimensions: list[int]):
+
         super().__init__()
         self.distributions = distributions
         self.dimensions = [0] + dimensions
 
 
-    def forward(self, num_samples, context=None):
+    def forward(self, num_samples):
+        """
+        Sample from the joint distribution.
+
+        Parameters
+        ----------
+        num_samples: int,
+            The number of samples to draw from the distribution.
+
+        Returns
+        -------
+        samples: torch.Tensor,
+            The samples drawn from the distribution.
+        """
         return torch.cat([d.forward(num_samples) for d in self.distributions], dim=1)
 
-    def log_prob(self, x):
+    def log_prob(self, x: torch.Tensor):
+        """
+        Get the log probability of a batch of samples.
+
+        Parameters
+        ----------
+        x: torch.Tensor,
+            The batch of samples.
+
+        Returns
+        -------
+        torch.Tensor,
+            The log probability of the samples.
+
+        """
         return sum(d.log_prob(x[:, self.dimensions[i]:self.dimensions[i+1]]) for i, d in enumerate(self.distributions))
