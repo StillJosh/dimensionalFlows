4f02fa3bc294719b925caf387ca02d0a8063d412
Joshua Stiller <joshua.stiller@tum.de>
2024-03-08 19:52:45 +0100

    MAINT: Restructured the training script to make it leaner

--- src/train.py
+++ src/train.py
@@ -6,30 +6,29 @@
 
 # Real NVP
 
-import yaml
 from pathlib import Path
-import os
 
-import torch
-import numpy as np
 import normflows as nf
-from matplotlib import pyplot as plt
+from normflows.core import NormalizingFlow
+import torch
+import yaml
 from tqdm import tqdm
+
 import wandb
 
-from src.utils.config_loader import get_architecture_from_config
-from src.utils.logger import logger
-from src.utils import utils
+from src.models.flows import norm_flow
+from src.utils.distributions import JointDistribution
 from src.plotting import distribution_plotting as dsplot
+from src.utils import utils
+from src.utils.config_loader import get_architecture_from_config, get_prior_from_config
+from src.utils.logger import logger
 
+# Disable wandb for debugging
 #os.environ["WANDB_MODE"] = "disabled"
 
 
-def train(config):
-
-    run = wandb.init(project='DimensionalFlows', config=config)
-    logger.info('Start Run')
-
+def train():
+    """ Load the model and dataset and train the normalizing flow. """
 
     # Use Apple M1 Chip if available, else check if GPU is available, else train on cpu
     if torch.backends.mps.is_available():
@@ -42,49 +41,60 @@ def train(config):
 
     logger.info(f'Using device: {device}')
 
-    
-    # Define flows
-    K = 16
-    torch.manual_seed(0)
-
-    latent_size = 2
-    b = torch.Tensor([1 if i % 2 == 0 else 0 for i in range(latent_size)])
-    flows = []
-    for i in range(K):
-        s = nf.nets.MLP([latent_size, 2 * latent_size, latent_size], init_zeros=True)
-        t = nf.nets.MLP([latent_size, 2 * latent_size, latent_size], init_zeros=True)
-        if i % 2 == 0:
-            flows += [nf.flows.MaskedAffineFlow(b, t, s)]
-        else:
-            flows += [nf.flows.MaskedAffineFlow(1 - b, t, s)]
-        flows += [nf.flows.ActNorm(latent_size)]
-
-    # Set target and q0
+    # Set target and base distribution
     target = nf.distributions.TwoModes(2, 0.1)
     q0 = get_prior_from_config(config, run)
 
+    # Sample from the target distribution
     x_train = utils.rejection_sampling_2d(lambda x: torch.exp(target.log_prob(x)), 1000, -3, 3, -2, 2)
 
-    # Construct flow model
-    nfm = nf.NormalizingFlow(q0=q0, flows=flows, p=target)
+    # Define the normalizing flow model
+    nfm = norm_flow(q0, 2, K=16)
 
     # Move model on GPU if available
     nfm = nfm.to(device)
     x_train = x_train.to(device)
-    #nfm = nfm.double()
 
-    nfm, criterion, optimizer, scheduler = get_architecture_from_config(nfm, config)
 
+    optimizer, scheduler = get_architecture_from_config(nfm, config)
+
+    # Iterate over the specified epochs, log the loss and regularly plot the current distribution as a contour plot
     for it in tqdm(range(config['max_iter'])):
         loss = run_epoch(nfm, x_train, optimizer, config)
         run.log({'loss': loss})
 
-        if (it + 1) % config['show_iter'] == 0:
-            fig, ax = dsplot.plot_progress_2d(target, nfm, device)
-            run.log({'chart': wandb.Image(fig)})
+        if (it + 1) % 1 == 0:  # config['show_iter'] == 0:
+            if x_train.shape[1] == 1:
+                fig, ax = dsplot.plot_progress_1d(x_train, nfm, device)
+                run.log({'chart': wandb.Image(fig)})
+
+            elif x_train.shape[1] == 2:
+                fig, ax = dsplot.plot_progress_2d(target, nfm, device)
+                run.log({'chart': wandb.Image(fig)})
+
+    torch.save(nfm, 'model.pth')
+    run.log_model('model.pth')
 
 
-def run_epoch(nfm, x_train, optimizer, config):
+def run_epoch(nfm: NormalizingFlow, x_train: torch.tensor, optimizer: torch.optim.Optimizer) -> float:
+    """
+    Run a single epoch of training.
+
+    Parameters
+    ----------
+    nfm: NormalizingFlow,
+        The normalizing flow model.
+    x_train: torch.tensor,
+        The training data.
+    optimizer: torch.optim.Optimizer,
+        The optimizer.
+
+    Returns
+    -------
+    float,
+        The loss of the model in this epoch.
+    """
+
 
     optimizer.zero_grad()
     if config['annealing']:
@@ -100,9 +110,15 @@ def run_epoch(nfm, x_train, optimizer, config):
     return loss.to('cpu').data.numpy()
 
 
-
 if __name__ == '__main__':
+    torch.manual_seed(0)
+
     # Load the run configurations from the config file
     config = yaml.load(open(Path(__file__).parent.joinpath('train_config.yaml'), 'r'),
                        Loader=yaml.FullLoader)
-    train(config)
+
+    # Start the wandb run
+    run = wandb.init(project='DimensionalFlows', config=config)
+    logger.info('Start Run')
+
+    train()
